9da0cb2c6f76008a44762364703343f6918c1ef6
OverKoder <theoverkoder@gmail.com>
2021-12-25 19:21:35 +0100

    Updated code, added utility functions and plot output

---
+++ main.py
@@ -0,0 +1,168 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+
+from model import VGG
+from utils import progress_bar, interval95
+
+import matplotlib.pyplot as plt
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+print('Building model..')
+model = VGG('VGG19')
+model = model.to(device)
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr= 1e-3)
+
+
+
+# Training function
+def train(epoch):
+    print('-----=| Epoch %d |=-----' % epoch)
+
+    # Set model to train
+    model.train()
+
+    train_loss = 0
+    correct = 0
+    total = 0
+
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    
+    return train_loss, 100.*correct/total
+
+
+def test(epoch):
+
+    global best_acc
+
+    #Set model to evaluation
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    
+    return test_loss, 100.*correct/total
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'model': model.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.pth')
+        best_acc = acc
+
+def main():
+
+    axis = list(range(start_epoch, start_epoch+200))
+    train_loss_list, test_loss_list = [], []
+    train_acc_list, test_acc_list = [], []
+    for epoch in range(start_epoch, start_epoch+200):
+
+        train_loss, train_acc = train(epoch)
+        train_loss_list.append(train_loss)
+        train_acc_list.append(train_acc)
+
+        test_loss, test_acc = test(epoch)
+        test_loss_list.append(test_loss)
+        test_acc_list.append(test_acc)       
+
+        scheduler.step()
+
+    plt.xlabel("Epochs")
+
+    plt.ylabel("Loss")
+    plt.plot(axis,train_loss_list,label= "Train loss")
+    plt.plot(axis,test_loss_list,label= "Test loss")
+    plt.legend()
+    plt.savefig("Loss")
+    plt.clf()
+
+    plt.ylabel("Accuracy (%)")
+    plt.plot(axis,train_acc_list,label= "Train accuracy")
+    plt.plot(axis,test_acc_list,label= "Test accuracy")
+    plt.legend()
+    plt.savefig("Accuracy")
+    plt.clf()
+
+    print("Final accuracy:", test_acc_list[-1])
+    interval = interval95( test_acc_list[-1] / 100, len(testset))
+    print("Confidence interval (95%):")
+    print("[",test_acc_list[-1] - interval[0], test_acc_list[-1] + interval[1], "]" )
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,107 @@
+import sys
+import time
+import os
+import math
+
+TOTAL_BAR_LENGTH = 65
+last_time = time.time()
+begin_time = last_time
+
+_, term_width = os.popen('stty size', 'r').read().split()
+term_width = int(term_width)
+
+def progress_bar(current, total, msg=None):
+    global last_time, begin_time
+    if current == 0:
+        begin_time = time.time()  # Reset for new bar.
+
+    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+
+    sys.stdout.write(' [')
+    for i in range(cur_len):
+        sys.stdout.write('=')
+    sys.stdout.write('>')
+    for i in range(rest_len):
+        sys.stdout.write('.')
+    sys.stdout.write(']')
+
+    cur_time = time.time()
+    step_time = cur_time - last_time
+    last_time = cur_time
+    tot_time = cur_time - begin_time
+
+    L = []
+    L.append('  Step: %s' % format_time(step_time))
+    L.append(' | Tot: %s' % format_time(tot_time))
+    if msg:
+        L.append(' | ' + msg)
+
+    msg = ''.join(L)
+    sys.stdout.write(msg)
+    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
+        sys.stdout.write(' ')
+
+    # Go back to the center of the bar.
+    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
+        sys.stdout.write('\b')
+    sys.stdout.write(' %d/%d ' % (current+1, total))
+
+    if current < total-1:
+        sys.stdout.write('\r')
+    else:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+def format_time(seconds):
+
+    # Just in case it takes a long time...
+    days = seconds // (3600/24)
+    seconds = seconds - days*3600*24
+
+    hours = seconds // 3600
+    seconds = seconds - hours*3600
+
+    minutes = seconds // 60
+    seconds = seconds - minutes*60
+
+    real_seconds = int(seconds)
+    seconds = seconds - real_seconds
+
+    millis = int(seconds*1000)
+
+    f = ''
+    # Maximum units to show is 2
+    max_units = 1
+    if days > 0:
+        f += str(days) + 'D '
+        max_units += 1
+
+    if hours > 0 and max_units <= 2:
+        f += str(hours) + 'h '
+        max_units += 1
+    if minutes > 0 and max_units <= 2:
+        f += str(minutes) + 'm '
+        max_units += 1
+    if real_seconds > 0 and max_units <= 2:
+        f += str(real_seconds) + 's '
+        max_units += 1
+    if millis > 0 and max_units <= 2:
+        f += str(millis) + 'ms'
+        max_units += 1
+
+    # If it took less than 1ms
+    if f == '':
+        f = '0ms'
+
+    return f
+
+def interval95(p,data):
+    """
+    Calculates confidence interval at 95%
+    """
+    
+    err = 1.96 * math.sqrt((p*(1-p)) / data)
+
+    return [p - err, p + err]
