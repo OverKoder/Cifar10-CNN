5da83ebe098ff26df7d28fcd9c341d526b7aba28
OverKoder <theoverkoder@gmail.com>
2021-12-25 19:49:26 +0100

    Fixed syntax error (oops)

--- main.py
+++ main.py
@@ -14,54 +14,6 @@ from utils import progress_bar, interval95
 
 import matplotlib.pyplot as plt
 
-# Device setup
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
-
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
-# Data
-print('Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
-
-# Model
-print('Building model..')
-model = VGG('VGG19')
-model = model.to(device)
-
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr= 1e-3)
-
-
-
 # Training function
 def train(epoch):
     print('-----=| Epoch %d |=-----' % epoch)
@@ -135,9 +87,53 @@ def test(epoch):
 
 def main():
 
+    # Device setup
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    best_acc = 0  # best test accuracy
+    start_epoch = 0 
+
+    # Data
+    print('Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+            'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # Model
+    print('Building model..')
+    model = VGG('VGG19')
+    model = model.to(device)
+    if device == 'cuda':
+        model = torch.nn.DataParallel(model)
+        cudnn.benchmark = True
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr= 1e-3)
+
     axis = list(range(start_epoch, start_epoch+200))
     train_loss_list, test_loss_list = [], []
     train_acc_list, test_acc_list = [], []
+
     for epoch in range(start_epoch, start_epoch+200):
 
         train_loss, train_acc = train(epoch)
@@ -169,5 +165,8 @@ def main():
     interval = interval95( best_acc / 100, len(testset))
     print("Confidence interval (95%):")
     print("[",best_acc - interval[0], best_acc + interval[1], "]" )
+
+
+
 if __name__ == "__main__":
     main()
